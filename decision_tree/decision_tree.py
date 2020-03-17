4f25149cbc819b065b9917ddb00e1afadac8b045
Yee Fay Lim <yeefay.lim@gmail.com>
2020-03-17 14:12:50 -0700

    Decision tree cost complexity pruning

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,5 +1,6 @@
 # Based loosely on https://github.com/SebastianMantey/Random-Forest-from-Scratch
 
+import cross_validation.cv as cv
 import numpy as np
 import pandas as pd
 import operator
@@ -38,6 +39,7 @@ class DecisionTree():
         self.max_depth = max_depth or (self.max_features and (2 * self.max_features))
         self.min_samples = min_samples or max(1, round(0.001 * len(self.data)))
         self.tree_dict = {}
+        self.pruned = False
 
     def _is_pure(self, obs_index):
         """
@@ -195,41 +197,68 @@ class DecisionTree():
         op = self.feature_comparators[feature]
         answer = tree[key][int(not(op(observation[feature], val)))]
         if not isinstance(answer, dict):
-            return answer
+            return answer[0]
         return self.predict_observation(observation, answer)
 
-    def predict(self, x):
+    def predict(self, x, subtree=None):
         """
-        Get predictions for whole dataset
+        Get predictions for given feature data
         :param x: (DataFrame) Feature data to predict on
         :return: (Series) Predictions
         """
-        return x.apply(self.predict_observation, args=(self.tree_dict,), axis=1)
+        subtree = subtree or self.tree_dict
+        if not subtree:
+            self.fit()
+        if not isinstance(subtree, dict):
+            return subtree[0]
+        return x.apply(self.predict_observation, args=(subtree,), axis=1)
 
-    def _grow(self, obs_index, counter=0, branches_must_differ=False):
-        """
-        Adds a branch to decision tree dictionary (self.tree_dict)
-        :param obs_index: absolute indices of observations to be considered
-        :param counter: (int) Current depth of branch
-        """
-        data = self.data.loc[obs_index]
-        if (counter == self.max_depth) or (self.is_classifier and self._is_pure(obs_index)):
-            return self._assign_leaf_val(obs_index)
-        else:
-            counter += 1
-            potential_splits = self._get_potential_splits(obs_index)
-            split_column, split_value = self._determine_best_split(potential_splits)
-            index_below, index_above = self._split_data(split_column, split_value)
-            if len(index_below) < self.min_samples or len(index_above) < self.min_samples:
-                return self._assign_leaf_val(obs_index)
-            yes_answer = self._grow(index_below, counter=counter)
-            no_answer = self._grow(index_above, counter=counter)
-            # In a random forest tree, if the answers are the same, then there is no point in asking the question.
-            # This could happen when the data is classified even though it is not pure yet.
-            if branches_must_differ and (yes_answer == no_answer):
-                return yes_answer
-            return { (split_column, split_value): [yes_answer, no_answer] }
+    def _get_min_cost_subtree(self, tree, alpha=0):
+        cost_method = self._get_rss
+        if self.is_classifier:
+            cost_method = self._get_entropy
+        if not isinstance(tree, dict):
+            return (tree, tree[1], cost_method(tree[1]), alpha)
+        branches = list(tree.values())[0]
+        min_cost_subtree_l = self._get_min_cost_subtree(branches[0], alpha)
+        min_cost_subtree_r = self._get_min_cost_subtree(branches[1], alpha)
+        obs_index = min_cost_subtree_l[1].union(min_cost_subtree_r[1])
+        leaf_cost = cost_method(obs_index)
+        branching_cost = min_cost_subtree_l[2] + min_cost_subtree_r[2]
+        branching_alpha = min_cost_subtree_l[3] + min_cost_subtree_r[3]
+        if self.is_classifier:
+            wt_l = len(min_cost_subtree_l[1]) * 1.0 / len(obs_index)
+            wt_r = len(min_cost_subtree_r[1]) * 1.0 / len(obs_index)
+            branching_cost = (wt_l * min_cost_subtree_l[2]) + (wt_r * min_cost_subtree_r[2])
+        if branching_cost + branching_alpha < leaf_cost + alpha:
+            return ({ list(tree.keys())[0]: [min_cost_subtree_l[0], min_cost_subtree_r[0]] }, obs_index, branching_cost, branching_alpha)
+        return ((self._assign_leaf_val(obs_index), obs_index), obs_index, leaf_cost, alpha)
 
-    def fit(self, branches_must_differ=False):
-        self.tree_dict = self._grow(self.data.index, 0, branches_must_differ)
+    def _prune(self, alpha=0):
+        if len(self.tree_dict) == 0:
+            self.fit()
+        return self._get_min_cost_subtree(self.tree_dict, alpha)[0]
+
+    def prune(self, alphas=np.arange(0, 2.2, 0.2), k=10):
+        """
+        Prunes a fitted decision tree
+        :param alphas: (numpy.ndarray) Alpha values for cost complexity pruning
+        :param k: (int) Number of folds to use in cross-validation for selecting alpha
+        """
+        if self.pruned and ((alphas.shape == self.pruning_alphas.shape) or (np.not_equal(alphas, self.pruning_alphas).sum() == 0)):
+            return
+        self.pruning_alphas = alphas
+        x, y = self.data[self.features], self.data.dependent
+        best_subtrees = pd.Series(alphas, index=alphas).apply(lambda alpha: self._prune(alpha))
+        self.fold_mean_errors = pd.DataFrame(index=range(1, k+1), columns=alphas)
+        for i in range(1, k+1):
+            exclude, include = cv.partition_indices(i, k, self.data.index)
+            tree_i = DecisionTree(x.loc[include], y.loc[include], max_features=self.max_features, max_depth=self.max_depth, min_samples=self.min_samples)
+            tree_i.fit()
+            best_subtrees_i = pd.Series(alphas, index=alphas).apply(lambda alpha: tree_i._prune(alpha))
+            self.fold_mean_errors.loc[i] = best_subtrees_i.apply(lambda tree: utils.get_mean_error(tree_i.predict(x.loc[exclude,:], tree),
+              y.loc[exclude], not(self.is_classifier)))
+        mean_FMEs = self.fold_mean_errors.mean().values
+        self.tree_dict = best_subtrees[alphas[np.where(mean_FMEs == mean_FMEs.min())[-1][-1]]]
+        self.pruned = True
 
