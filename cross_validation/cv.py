a3fa0f2efb3704282c15bae96aaf83e3eba570c5
Yee Fay Lim <yeefay.lim@gmail.com>
2020-03-17 14:00:37 -0700

    Allow option for standardising input (or not), and keyword args, in cross-validation methods

--- cross_validation/cv.py
+++ cross_validation/cv.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.utils import shuffle
 
 # Function definitions to avoid recomputing moments for each data subset
 
@@ -21,41 +22,55 @@ def get_shuffled_data(x, y):
     shuffled = shuffle(data).reset_index()
     return shuffled[features], shuffled.dependent
 
+def partition_indices(i, k, index):
+    if i < 1 or i > k:
+        raise ValueError
+    step, n = 1.0 / k, len(index)
+    lb, ub = round((i-1) * step * n), round(i * step * n)
+    include = list(range(lb))
+    include.extend(range(ub, n))
+    exclude = list(range(lb, ub))
+    return index[exclude], index[include]
+
 # Leave-one-out cross validation
-# Example: loocv(LogisticRegression(multi_class='multinomial', solver='newton-cg'), x, y)
-def loocv(x, y, model, shuffle=True):
+# Example: loocv(x, y, LogisticRegression(multi_class='multinomial', solver='newton-cg'))
+def loocv(x, y, model, shuffle=True, standardise=True, **kwargs):
     if shuffle:
         x, y = get_shuffled_data(x, y)
     n = x.shape[0] # Assumes x.shape[0] == y.shape[0]
-    x_means = x.mean()
-    # Pandas gives adjusted stderr; need unadjusted to avoid taking sqrt of negative number
-    x_stderrs = np.sqrt(x.std()**2 * (n-1)/n)
+    if standardise:
+        x_means = x.mean()
+        # Pandas gives adjusted stderr; need unadjusted to avoid taking sqrt of negative number
+        x_stderrs = np.sqrt(x.std()**2 * (n-1)/n)
     predictions = np.full(n, -1)
     errors = np.zeros(n)
     for i in range(n):
-        fitted = model.fit(restandardise_wo_obs(x[x.index != i], x_means, x_stderrs, n, x.loc[i]), y[y.index != i])
+        x_fit = x[x.index != i]
+        if standardise:
+            x_fit = restandardise_wo_obs(x[x.index != i], x_means, x_stderrs, n, x.loc[i])
+        fitted = model.fit(x_fit, y[y.index != i], **kwargs)
         predictions[i] = fitted.predict(np.reshape(x.loc[i].values, (1, -1)))[0]
         errors[i] = int(predictions[i] != y[i])
     cv_error = errors.mean()
     return (predictions, errors, cv_error)
 
-def k_fold_cv(x, y, k, model, shuffle=True):
+def k_fold_cv(x, y, k, model, shuffle=True, standardise=True, **kwargs):
     if shuffle:
         x, y = get_shuffled_data(x, y)
     n = x.shape[0] # Assumes x.shape[0] == y.shape[0]
     x_means = x.mean()
     x_stderrs = np.sqrt(x.std()**2 * (n-1)/n)
-    i, step = 0, int(n * 1.0/k)
     predictions = np.full(n, -1)
     errors = np.zeros(n)
-    fold_MSEs = np.zeros(step)
-    for i in range(0, n, step):
-        include = list(range(i))
-        include.extend(range(i + 15, n))
-        exclude = range(i, i + 15)
-        fitted = model.fit(restandardise_wo_obs(x.loc[include], x_means, x_stderrs, n, x.loc[exclude]), y.loc[include])
+    fold_MSEs = np.zeros(k)
+    for i in range(1, k+1):
+        exclude, include = partition_indices(i, k, x.index)
+        x_fit = x.loc[include]
+        if standardise:
+            x_fit = restandardise_wo_obs(x.loc[include], x_means, x_stderrs, n, x.loc[exclude])
+        fitted = model.fit(x_fit, y.loc[include], **kwargs)
         predictions[exclude] = fitted.predict(x.loc[exclude].values)
         errors[exclude] = (predictions[exclude] != y.values[exclude]).astype(int)
-        fold_MSEs[int(i/step)] = errors[exclude].mean()
+        fold_MSEs[i-1] = errors[exclude].mean()
     cv_error = fold_MSEs.mean()
     return (predictions, errors, cv_error)
