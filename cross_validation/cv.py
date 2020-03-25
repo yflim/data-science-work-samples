161a18397ed013d82094d1a998abf2bee1bff739
Yee Fay Lim <yeefay.lim@gmail.com>
2020-03-25 14:15:30 -0700

    1. Add intercept and verbosity parameters. 2. Normalise validation fold data too (if specified)!

--- cross_validation/cv.py
+++ cross_validation/cv.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import add_dummy_feature
 from sklearn.utils import shuffle
 
 # Function definitions to avoid recomputing moments for each data subset
@@ -20,6 +21,7 @@ def get_shuffled_data(x, y):
     data = x
     data['dependent'] = y
     shuffled = shuffle(data).reset_index()
+    data.drop(columns='dependent')
     return shuffled[features], shuffled.dependent
 
 def partition_indices(i, k, index):
@@ -48,13 +50,18 @@ def loocv(x, y, model, shuffle=True, standardise=True, **kwargs):
         x_fit = x[x.index != i]
         if standardise:
             x_fit = restandardise_wo_obs(x[x.index != i], x_means, x_stderrs, n, x.loc[i])
+        if intercept:
+            x_fit = add_dummy_feature(x_fit)
         fitted = model.fit(x_fit, y[y.index != i], **kwargs)
-        predictions[i] = fitted.predict(np.reshape(x.loc[i].values, (1, -1)))[0]
+        x_predict = np.reshape(x.loc[i].values, (1, -1))
+        if intercept:
+            x_predict = add_dummy_feature(x_predict)
+        predictions[i] = fitted.predict(x_predict)[0]
         errors[i] = int(predictions[i] != y[i])
     cv_error = errors.mean()
     return (predictions, errors, cv_error)
 
-def k_fold_cv(x, y, k, model, shuffle=True, standardise=True, **kwargs):
+def k_fold_cv(x, y, k, model, cv_verbose=False, shuffle=True, intercept=True, standardise=True, **kwargs):
     if shuffle:
         x, y = get_shuffled_data(x, y)
     n = x.shape[0] # Assumes x.shape[0] == y.shape[0]
@@ -64,12 +71,20 @@ def k_fold_cv(x, y, k, model, shuffle=True, standardise=True, **kwargs):
     errors = np.zeros(n)
     fold_MSEs = np.zeros(k)
     for i in range(1, k+1):
+        if cv_verbose:
+            print('fold ', i)
         exclude, include = partition_indices(i, k, x.index)
         x_fit = x.loc[include]
         if standardise:
             x_fit = restandardise_wo_obs(x.loc[include], x_means, x_stderrs, n, x.loc[exclude])
+        if intercept:
+            x_fit = add_dummy_feature(x_fit)
         fitted = model.fit(x_fit, y.loc[include], **kwargs)
-        predictions[exclude] = fitted.predict(x.loc[exclude].values)
+        exclude_len = len(x.loc[exclude])
+        exclude_std = ((x.loc[exclude] - x.loc[exclude].mean()) / np.sqrt(x.loc[exclude].std()**2 * (exclude_len-1)/exclude_len)).values
+        if intercept:
+            exclude_std = add_dummy_feature(exclude_std)
+        predictions[exclude] = fitted.predict(exclude_std)
         errors[exclude] = (predictions[exclude] != y.values[exclude]).astype(int)
         fold_MSEs[i-1] = errors[exclude].mean()
     cv_error = fold_MSEs.mean()
